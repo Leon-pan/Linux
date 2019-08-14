02bc02255535659f97996bc68b501c0e17ce003b
whosyourmami <724517470@qq.com>
2019-08-14 14:45:55 +0800

    update

--- ansible/ansible.sh
+++ ansible/ansible.sh
@@ -1,5 +1,5 @@
 vi /etc/ansible/hosts
-
+ansible_ssh_user="administrator" ansible_ssh_pass="Hleast@2019" ansible_ssh_port=5985 ansible_connection="winrm" ansible_winrm_server_cert_validation=ignore
 
 #ping测试
 ansible windows -m win_ping
@@ -30,3 +30,12 @@ ansible linux -m shell -a "/tmp/hl_bizdemo/bin/updateServer.sh"
 cp /usr/lib/python2.7/site-packages/winrm/protocol.py{,.bak}
 sed -i "s#tdout_buffer.append(stdout)#tdout_buffer.append(stdout.decode('gbk').encode('utf-8'))#g" /usr/lib/python2.7/site-packages/winrm/protocol.py
 sed -i "s#stderr_buffer.append(stderr)#stderr_buffer.append(stderr.decode('gbk').encode('utf-8'))#g" /usr/lib/python2.7/site-packages/winrm/protocol.py
+
+
+#linux java软连接
+ln -s $JAVA_HOME/bin/java /usr/bin/java
+ln -s $JAVA_HOME/bin/jar /usr/bin/jar
+
+
+#windows PATH追加
+ansible windows -m win_path -a "name=PATH elements=%JAVA_HOME%\\\bin;%JAVA_HOME%\jre\\\bin scope=machine state=present"
