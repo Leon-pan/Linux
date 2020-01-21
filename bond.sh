33b9f4e49a46e7d4c85cb51f7ca5305be5599939
Administrator <pjl@hleast.com>
2020-01-21 09:53:46 +0800

    update

--- bond.sh
+++ bond.sh
@@ -2,6 +2,7 @@
 #创建一个名为bond0的链路接口
 IP=10.147.111.11
 GATE=10.147.111.254
+MASK=24
 ETH1=enp4s0f0
 ETH2=enp4s0f1
 #ETH3=eno3
@@ -16,7 +17,7 @@ BOOTPROTO=static
 USERCTL=no
 ONBOOT=yes
 IPADDR=$IP
-PREFIX=24
+PREFIX=$MASK
 GATEWAY=$GATE
 BONDING_OPTS="mode=0 miimon=100"
 EOF
@@ -63,7 +64,10 @@ EOF
 #SLAVE=yes
 #EOF
 systemctl restart network
-ping $GATE -c 1
+ping $GATE -c 10
+#mode=0，表示load balancing (round-robin)为负载均衡方式，两块网卡都工作，但是与网卡相连的交换必须做特殊配置（ 这两个端口应该采取聚合方式），因为做bonding的这两块网卡是使用同一个MAC地址
+#mode=6，表示load balancing (round-robin)为负载均衡方式，两块网卡都工作，但是该模式下无需配置交换机，因为做bonding的这两块网卡是使用不同的MAC地址
+#mode=1，表示fault-tolerance (active-backup)提供冗余功能，工作方式是主备的工作方式，也就是说默认情况下只有一块网卡工作,另一块做备份
 #reboot
 #modprobe bonding
 #lsmod | grep bonding
