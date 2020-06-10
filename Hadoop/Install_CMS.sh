c9eae2752af37f13fc8eb88991506bb9727f18c0
Administrator <pjl@hleast.com>
2020-06-10 11:30:44 +0800

    jdk url update

--- Hadoop/Install_CMS.sh
+++ Hadoop/Install_CMS.sh
@@ -113,7 +113,7 @@ Install_JAVA() {
 		echo -e "${RED}当前目录未找到jdk-8u*.tar.gz${RES}"
 		#jdk1.8下载链接失效
 		#yum -y install wget
-		#wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u171-b11/512cd62ec5174c3487ac17c61aaa89e8/jdk-8u171-linux-x64.tar.gz"
+		#wget --no-cookies --no-check-certificate "http://public-repo-1.hortonworks.com/ARTIFACTS/jdk-8u60-linux-x64.tar.gz"
 	fi
 }
 
