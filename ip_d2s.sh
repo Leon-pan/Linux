e09da2f5785ced40eac57dd8411d1b236c38d120
Leon-pan <724517470@qq.com>
2020-08-25 15:36:30 +0800

    update

--- ip_d2s.sh
+++ ip_d2s.sh
@@ -15,7 +15,10 @@ d2s() {
 	else
 		echo "当前为动态地址，开始执行脚本"
 	fi
-	ip=$(ip addr | awk '/^[0-9]+: / {}; /inet.*global/ {print gensub(/(.*)\/(.*)/, "\\1", "g", $2)}' | grep -v 0.1)
+	#ip=$(ip addr | awk '/^[0-9]+: / {}; /inet.*global/ {print gensub(/(.*)\/(.*)/, "\\1", "g", $2)}' | grep -v 0.1)
+	#获取默认网关，然后获取用于与该网关通信的IP地址
+	ip=$(ip route get $(ip route show 0.0.0.0/0 | grep -oP 'via \K\S+') | grep -oP 'src \K\S+')
+	echo "获取到的IP地址为：$ip"
 	if ! grep "IPADDR" $network_file > /dev/null 2>&1; then
 		sed -i '/^BOOTPROTO=/c\BOOTPROTO=static' $network_file
 		cat >> $network_file <<- EOF
