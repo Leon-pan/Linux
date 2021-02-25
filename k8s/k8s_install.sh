ba05e8d5855b734c7c452a4db596e46d0df63974
Leon-pan <724517470@qq.com>
2021-02-25 10:32:30 +0800

    update

--- k8s/k8s_install.sh
+++ k8s/k8s_install.sh
@@ -1,16 +1,31 @@
 #https://kubernetes.io/zh/docs/setup/production-environment/tools/kubeadm/install-kubeadm/
-#安装docker，k8s官方建议安装docker18.09版本
-## Create /etc/docker
-sudo mkdir /etc/docker
+#关闭防火墙、swap分区等
+systemctl disable --now firewalld
+setenforce 0
+sed -i '/^SELINUX=/c\SELINUX=disabled' /etc/selinux/config
+swapoff -a && sysctl -w vm.swappiness=0
+sed -ri '/^[^#]*swap/s@^@#@' /etc/fstab
+#NetworkManage可能会干扰Calico代理正确路由的能力,建议关闭
+systemctl disable --now NetworkManager
 
+
+#安装docker，k8s官方建议安装docker18.09版本或19.03版本
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+sed -i 's+download.docker.com+mirrors.aliyun.com/docker-ce+' /etc/yum.repos.d/docker-ce.repo
+yum install -y docker-ce-19.03.*
+
+## Create /etc/docker
+mkdir /etc/docker
 
 # Set up the Docker daemon
-cat <<EOF | sudo tee /etc/docker/daemon.json
+cat <<EOF | tee /etc/docker/daemon.json
 {
   "exec-opts": ["native.cgroupdriver=systemd"],
   "log-driver": "json-file",
   "log-opts": {
-    "max-size": "100m"
+    "max-size": "100m",
+    "max-file": "3"
   },
   "storage-driver": "overlay2",
   "storage-opts": [
@@ -19,16 +34,12 @@ cat <<EOF | sudo tee /etc/docker/daemon.json
 }
 EOF
 
-
-# Create /etc/systemd/system/docker.service.d
-sudo mkdir -p /etc/systemd/system/docker.service.d
-
-
 # Restart Docker
-sudo systemctl daemon-reload
-sudo systemctl restart docker
-sudo systemctl enable docker
+systemctl daemon-reload
+systemctl enable --now docker
 
+
+#使用阿里云镜像的k8s源
 cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
@@ -39,11 +50,98 @@ repo_gpgcheck=0
 gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
 EOF
 
-# 将 SELinux 设置为 permissive 模式（相当于将其禁用）
-setenforce 0
-sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
 
-yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+#系统调优
+cat <<EOF >> /etc/sysctl.conf
+net.bridge.bridge-nf-call-ip6tables=1
+net.bridge.bridge-nf-call-iptables=1
+net.ipv4.ip_forward=1
+net.ipv4.conf.all.forwarding=1
+net.ipv4.neigh.default.gc_thresh1=4096
+net.ipv4.neigh.default.gc_thresh2=6144
+net.ipv4.neigh.default.gc_thresh3=8192
+net.ipv4.neigh.default.gc_interval=60
+net.ipv4.neigh.default.gc_stale_time=120
+
+# 参考 https://github.com/prometheus/node_exporter#disabled-by-default
+kernel.perf_event_paranoid=-1
+
+#sysctls for k8s node config
+net.ipv4.tcp_slow_start_after_idle=0
+net.core.rmem_max=16777216
+fs.inotify.max_user_watches=524288
+kernel.softlockup_all_cpu_backtrace=1
+
+kernel.softlockup_panic=0
+
+kernel.watchdog_thresh=30
+fs.file-max=2097152
+fs.inotify.max_user_instances=8192
+fs.inotify.max_queued_events=16384
+vm.max_map_count=262144
+fs.may_detach_mounts=1
+net.core.netdev_max_backlog=16384
+net.ipv4.tcp_wmem=4096 12582912 16777216
+net.core.wmem_max=16777216
+net.core.somaxconn=32768
+net.ipv4.ip_forward=1
+net.ipv4.tcp_max_syn_backlog=8096
+net.ipv4.tcp_rmem=4096 12582912 16777216
+
+net.ipv6.conf.all.disable_ipv6=1
+net.ipv6.conf.default.disable_ipv6=1
+net.ipv6.conf.lo.disable_ipv6=1
+
+kernel.yama.ptrace_scope=0
+vm.swappiness=0
+
+# 可以控制core文件的文件名中是否添加pid作为扩展。
+kernel.core_uses_pid=1
+
+# Do not accept source routing
+net.ipv4.conf.default.accept_source_route=0
+net.ipv4.conf.all.accept_source_route=0
+
+# Promote secondary addresses when the primary address is removed
+net.ipv4.conf.default.promote_secondaries=1
+net.ipv4.conf.all.promote_secondaries=1
+
+# Enable hard and soft link protection
+fs.protected_hardlinks=1
+fs.protected_symlinks=1
+
+# 源路由验证
+# see details in https://help.aliyun.com/knowledge_detail/39428.html
+net.ipv4.conf.all.rp_filter=0
+net.ipv4.conf.default.rp_filter=0
+net.ipv4.conf.default.arp_announce = 2
+net.ipv4.conf.lo.arp_announce=2
+net.ipv4.conf.all.arp_announce=2
+
+# see details in https://help.aliyun.com/knowledge_detail/41334.html
+net.ipv4.tcp_max_tw_buckets=5000
+net.ipv4.tcp_syncookies=1
+net.ipv4.tcp_fin_timeout=30
+net.ipv4.tcp_synack_retries=2
+kernel.sysrq=1
+
+#ipvs https://blog.csdn.net/bh1231/article/details/100947990
+net.netfilter.nf_conntrack_max=1048576
+net.nf_conntrack_max=1048576
+EOF
+
+sysctl -p
+
+#nofile
+cat >> /etc/security/limits.conf <<EOF
+* soft nofile 65535
+* hard nofile 65536
+EOF
+
+
+#安装k8s组件，这里选用1.18版本
+yum list kubeadm.x86_64 --showduplicates
+yum install -y kubeadm-1.18.* kubelet-1.18.* kubectl-1.18.* #--disableexcludes=kubernetes禁掉除此外的其他仓库
 
 systemctl enable --now kubelet
 
@@ -72,61 +170,104 @@ EOF
 
 chmod 755 /etc/sysconfig/modules/ipvs.modules && bash /etc/sysconfig/modules/ipvs.modules && lsmod | grep -e ip_vs -e nf_conntrack_ipv4
 
+
+#集群配置文件
+cat > init.yaml <<- 'EOF'
+apiVersion: kubeadm.k8s.io/v1beta2
+bootstrapTokens:
+- groups:
+  - system:bootstrappers:kubeadm:default-node-token
+  token: 7t2weq.bjbawausm0jaxury
+  ttl: 24h0m0s
+  usages:
+  - signing
+  - authentication
+kind: InitConfiguration
+localAPIEndpoint:
+  advertiseAddress: 1.2.3.4 #主节点IP
+  bindPort: 6443
+nodeRegistration:
+  criSocket: /var/run/dockershim.sock
+  name: k8s-master01 #主节点hostname
+  taints:
+  - effect: NoSchedule
+    key: node-role.kubernetes.io/master
+---
+apiServer:
+  timeoutForControlPlane: 4m0s
+apiVersion: kubeadm.k8s.io/v1beta2
+certificatesDir: /etc/kubernetes/pki
+clusterName: kubernetes
+controlPlaneEndpoint: LOAD_BALANCER_DNS:LOAD_BALANCER_PORT #主节点高可用
+controllerManager: {}
+dns:
+  type: CoreDNS
+etcd:
+  local:
+    dataDir: /var/lib/etcd
+imageRepository: registry.cn-hangzhou.aliyuncs.com/google_containers #使用阿里云镜像
+kind: ClusterConfiguration
+kubernetesVersion: v1.18.16 #版本号，同kubeadm版本
+networking:
+  dnsDomain: cluster.local
+  podSubnet: 172.168.0.0/16 #pod网段
+  serviceSubnet: 10.96.0.0/12 #svc网段
+scheduler: {}
+EOF
+
 #使用kubeadm安装时在配置文件添加以下部分使用ipvs
-...
-kubeProxy:
-  config:
-    mode: ipvs
-...
-
-kubeadm init --config <path_to_configuration_file>
+---
+apiVersion: kubeproxy.config.k8s.io/v1alpha1
+kind: KubeProxyConfiguration
+mode: ipvs
 
 
-#拉镜像，再重新tag
-docker pull gotok8s/kube-apiserver:v1.20.1
-docker pull gotok8s/kube-controller-manager:v1.20.1
-docker pull gotok8s/kube-scheduler:v1.20.1
-docker pull gotok8s/kube-proxy:v1.20.1
-docker pull gotok8s/pause:3.2
-docker pull gotok8s/etcd:3.4.13-0
-docker pull coredns/coredns:1.7.0
-
-docker tag gotok8s/kube-apiserver:v1.20.1 k8s.gcr.io/kube-apiserver:v1.20.1
-docker tag gotok8s/kube-controller-manager:v1.20.1 k8s.gcr.io/kube-controller-manager:v1.20.1
-docker tag gotok8s/kube-scheduler:v1.20.1 k8s.gcr.io/kube-scheduler:v1.20.1
-docker tag gotok8s/kube-proxy:v1.20.1 k8s.gcr.io/kube-proxy:v1.20.1
-docker tag gotok8s/pause:3.2 k8s.gcr.io/pause:3.2
-docker tag gotok8s/etcd:3.4.13-0 k8s.gcr.io/etcd:3.4.13-0
-docker tag coredns/coredns:1.7.0  k8s.gcr.io/coredns:1.7.0
+#提前下载镜像，节省初始化时间
+kubeadm config images pull --config init.yaml
 
 
-#初始化控制平面
-kubeadm init --control-plane-endpoint "k8s.citydo.com.cn:6443" --upload-certs --pod-network-cidr 10.244.0.0/16
+#初始化集群
+kubeadm init --config init.yaml --upload-certs
 
 
-#设置开机自启，并初始化
-systemctl enable kubelet.service
-kubeadm init --config kubeadm.yaml
-
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-#从节点加入集群
-kubeadm join 10.1.70.88:6443 --token zgmtzc.i4tvwr7a0jh6zvum \
-    --discovery-token-ca-cert-hash sha256:96a13e39ac446c55260249014161b5c846c55491e0c933f223f36676aff15d56
-
-#加入集群的格式为
-kubeadm join <master-ip>:<master-port> --token <token> --discovery-token-ca-cert-hash sha256:<hash>
-
-#token
-kubeadm token list
-
-#sha256
-openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //'
+#配置环境变量，用于访问Kubernetes集群
+cat <<EOF >> /root/.bashrc
+export KUBECONFIG=/etc/kubernetes/admin.conf
+EOF
+source /root/.bashrc
 
 
+#Calico安装
+#修改calico-etcd.yaml
+sed -i 's#etcd_endpoints: "http://<ETCD_IP>:<ETCD_PORT>"#etcd_endpoints: "https://主节点1IP:2379,https://主节点1IP:2379"#g' calico-etcd-v3.16.8.yaml
 
+ETCD_CA=`cat /etc/kubernetes/pki/etcd/ca.crt | base64 | tr -d '\n'`
+ETCD_CERT=`cat /etc/kubernetes/pki/etcd/server.crt | base64 | tr -d '\n'`
+ETCD_KEY=`cat /etc/kubernetes/pki/etcd/server.key | base64 | tr -d '\n'`
+sed -i "s@# etcd-key: null@etcd-key: ${ETCD_KEY}@g; s@# etcd-cert: null@etcd-cert: ${ETCD_CERT}@g; s@# etcd-ca: null@etcd-ca: ${ETCD_CA}@g" calico-etcd-v3.16.8.yaml
+
+sed -i 's#etcd_ca: ""#etcd_ca: "/calico-secrets/etcd-ca"#g; s#etcd_cert: ""#etcd_cert: "/calico-secrets/etcd-cert"#g; s#etcd_key: "" #etcd_key: "/calico-secrets/etcd-key" #g' calico-etcd-v3.16.8.yaml
+
+POD_SUBNET=`cat /etc/kubernetes/manifests/kube-controller-manager.yaml | grep cluster-cidr= | awk -F= '{print $NF}'`
+
+sed -i 's@# - name: CALICO_IPV4POOL_CIDR@- name: CALICO_IPV4POOL_CIDR@g; s@#   value: "192.168.0.0/16"@  value: '"${POD_SUBNET}"'@g' calico-etcd-v3.16.8.yaml
+
+#创建calico
+kubectl apply -f calico-etcd-v3.16.8.yaml
+
+
+#安装Metrics Server
+#将主节点的front-proxy-ca.crt复制到所有节点
+scp /etc/kubernetes/pki/front-proxy-ca.crt 其他节点:/etc/kubernetes/pki/front-proxy-ca.crt
+kubectl create -f metrics-server-v0.4.2.yml
+
+
+#安装ingress
+kubectl apply -f ingress-nginx-controller-v0.44.0.yml
+
+
+#安装国产dashboard
+https://www.kuboard.cn/install/install-dashboard.htm
 
 
 #查看节点和容器状态
@@ -145,50 +286,14 @@ kubectl get svc,pod -o wide
 / # nslookup kubernetes
 
 
-#检查kube-proxy 是否为IPVS 模式
+#开启ipvs
+#修改ConfigMap的kube-system/kube-proxy中的config.conf，把 mode: "" 改为mode: “ipvs" 保存退出即可
+kubectl edit cm kube-proxy -n kube-system
+
+###删除之前的proxy pod
+kubectl get pod -n kube-system |grep kube-proxy |awk '{system("kubectl delete pod "$1" -n kube-system")}'
+
+#查看日志,如果有 `Using ipvs Proxier.` 说明kube-proxy的ipvs 开启成功!
 kubectl logs -n kube-system kube-proxy-xxxx
 
 I0903 06:47:33.569723       1 server_others.go:170] Using ipvs Proxier.
-
-
-#Kubernetes Dashboard安装
-kubectl apply -f kubernetes-dashboard.yaml
-
-kubectl proxy
-
-#添加admin用户
-vi dashboard-adminuser.yaml
-apiVersion: v1
-kind: ServiceAccount
-metadata:
-  name: admin-user
-  namespace: kube-system
-
-kubectl create -f dashboard-adminuser.yaml
-
-#绑定admin用户组
-vi admin-user-role-binding.yaml
-apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRoleBinding
-metadata:
-  name: admin-user
-roleRef:
-  apiGroup: rbac.authorization.k8s.io
-  kind: ClusterRole
-  name: cluster-admin
-subjects:
-- kind: ServiceAccount
-  name: admin-user
-  namespace: kube-system
-
-kubectl create -f admin-user-role-binding.yaml
-
-#访问URL
-http://localhost:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/
-
-#获取token
-kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep admin-user | awk '{print $1}')
-
-kubectl proxy --address='0.0.0.0'  --accept-hosts='^*$'
-
-eyJhbGciOiJSUzI1NiIsImtpZCI6IiJ9.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJrdWJlLXN5c3RlbSIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VjcmV0Lm5hbWUiOiJhZG1pbi11c2VyLXRva2VuLXdycWxnIiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZXJ2aWNlLWFjY291bnQubmFtZSI6ImFkbWluLXVzZXIiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC51aWQiOiIwYWZhMTk4MS1hNWQ3LTRiZDItOGY3Ni1jOTZiYTA3MzFmYWUiLCJzdWIiOiJzeXN0ZW06c2VydmljZWFjY291bnQ6a3ViZS1zeXN0ZW06YWRtaW4tdXNlciJ9.mfN7QOkZy6I4VJWGQNFPcDy7Wdv4N7EQzaOmyV-PLAVFTPNekfgJbUZu3ZBAa5VoH_OudXRKgT9ZnIlQwxSWJyENQyWrYt_2qcySF0xB8-TmxeNRyTkDIX44bocuTaqbVAkCyS1M3QOPga7DTBXEd9k65hRvmRbYMF5c8LDjoIlVlO51Pql8-vzLwR5Yo7PWKgQqr_gw6LHe6ciwJ7plVfsVDH7nM9LSAOeuvEvzcre0ERcunDh6ueGbBrC3vWOqRH6-Gl1mucsRC_e-WIqOYx78Q7nkB-48ynVtGfoy6cFdzryNIPvyRvYqO4y7_VO6O3uZo6rU3_o4jrSq0jFqfQ
